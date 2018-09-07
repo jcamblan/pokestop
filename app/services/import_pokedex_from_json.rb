2bc34612d59e0ba651a91d83a21cb5120dfd6411
Julien Camblan <julien.camblan@gmail.com>
2018-09-07 12:09:56 +0200

    intégration bootstrap

--- app/services/import_pokedex_from_json.rb
+++ app/services/import_pokedex_from_json.rb
@@ -108,9 +108,9 @@ class ImportPokedexFromJson
       check_db = Evolution.where(pokemon_id: pokemon.id)
       next_evos = p.dig('next_evolution')
 
-      if check_db.empty?
+      if check_db.empty? #il faut vérifier que les évolutions qu'on va ajouter n'existent pas déjà
 
-        next_evos.each do |e| #il faut vérifier que les évolutions qu'on va ajouter n'existent pas déjà
+        next_evos.each do |e| 
 
           Evolution.create(pokemon_id: pokemon_id, after_evolution: after_evolution)
 
