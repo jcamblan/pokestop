44af31d360b83d4fb4eb29cdfd05e7aaacf0fe85
Julien Camblan <julien.camblan@gmail.com>
2018-09-11 15:24:33 +0200

    grosse refacto controller+model pour l'affichage des évolutions

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -10,58 +10,11 @@ class PokemonController < ApplicationController
 
   end
 
-
-  def evolutions_block(id)
-
-    block_evolutions = Array.new
-
-    pokemon = @pokemon
-
-    if pokemon.can_evolve? || pokemon.is_evolution?
-
-      first_form = Evolution.where(pokemon_id: id).or(Evolution.where(after_evolution: id)).first.first_form
-
-      block_evolutions << {
-        id: first_form,
-        name: Pokemon.find(first_form).name,
-        evolutions: get_evolutions(first_form)
-      }
-
-    end
-
-    return block_evolutions
-  	
-  end
-
-  def get_evolutions(id)
-    evolutions = Evolution.where(pokemon_id: id)
-
-    next_forms = Array.new
-
-    unless evolutions.nil?
-
-      evolutions.each do |e|
-
-        next_forms << {
-          id: e.after_evolution,
-          name: Pokemon.find(e.after_evolution).name,
-          evolutions: get_evolutions(e.after_evolution)
-        } 
-        
-      end
-      
-    end
-
-    return next_forms
-  end
-
-
   def show
 
   	@pokemon = Pokemon.where(name: params[:name]).first
 
-  	@evolutions = evolutions_block(@pokemon.id)
-
+  	@evolutions = @pokemon.first_form if @pokemon.can_evolve? || @pokemon.is_evolution?
 
   end
 
--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -20,6 +20,21 @@ class Pokemon < ApplicationRecord
   	end
   end
 
+  def first_form
+    first_form_id = Evolution.where(pokemon_id: id).or(Evolution.where(after_evolution: id)).first.first_form
+    pokemon = Pokemon.where(id: first_form_id)
+    return pokemon
+  end
+
+  def evolutions_as_pokemons
+    evolutions = Evolution.where(pokemon_id: id)
+    evolutions_array = Array.new
+    evolutions.each do |e|
+      evolutions_array << Pokemon.find(e.after_evolution)
+    end
+    return evolutions_array
+  end
+
   belongs_to :generation
   has_and_belongs_to_many :types
   has_many :evolutions
