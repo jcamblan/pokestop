2a23a3d617105e181f77d5575583423ef9fb572b
Julien Camblan <julien.camblan@gmail.com>
2018-09-14 15:47:05 +0200

    nouvel export

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,21 +1,11 @@
 class PokemonController < ApplicationController
 
-  def share
-
-  	list = Json.new
-
-  	list.create
-
-  	render json: list
-
+  def index
   end
 
   def show
-
-  	@pokemon = Pokemon.find(params[:id])
-
-  	@evolutions = @pokemon.first_form if @pokemon.can_evolve? || @pokemon.is_evolution?
-
+    @pokemon = Pokemon.find(params[:id])
+    @evolutions = @pokemon.first_form if @pokemon.can_evolve? || @pokemon.is_evolution?
   end
 
 end
