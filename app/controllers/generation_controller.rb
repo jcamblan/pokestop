23f76fdd91bcc537e7932ccce3f74b8300153f1a
Julien Camblan <julien.camblan@gmail.com>
2018-09-24 14:52:50 +0200

    suite des ajouts de locales + ajouts de titres aux pages

--- app/controllers/generation_controller.rb
+++ app/controllers/generation_controller.rb
@@ -1,5 +1,6 @@
 class GenerationController < ApplicationController
   def show
+    @generation = Generation.find(params[:id])
     @pokemons = Pokemon.where(generation_id: params[:id]).order(params[:order]).order(:num).page(params[:page]).per(25)
   end
   def index
