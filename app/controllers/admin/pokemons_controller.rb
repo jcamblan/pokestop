d3d8088d09c7add1a81df9f743a2810fdad9502e
Julien Camblan <julien.camblan@gmail.com>
2018-09-29 22:00:18 +0200

    les erreurs s'affichent mais les associations ne sont plus mises à jour

--- app/controllers/admin/pokemons_controller.rb
+++ app/controllers/admin/pokemons_controller.rb
@@ -14,28 +14,12 @@ class Admin::PokemonsController < ApplicationController
   end
 
   def update
-    pokemon = Pokemon.find(params[:id])
-    pokemon.num = params[:pokemon][:num]
-    pokemon.name_en = params[:pokemon][:name_en]
-    pokemon.pc_max = params[:pokemon][:pc_max]
-    pokemon.atk = params[:pokemon][:atk]
-    pokemon.def = params[:pokemon][:def]
-    pokemon.sta = params[:pokemon][:sta]
-    pokemon.generation_id = params[:pokemon][:generation_id]
-    pokemon.candy_id = params[:pokemon][:candy_id]
-    pokemon.candy_distance = params[:pokemon][:candy_distance]
-    pokemon.capture_rate = params[:pokemon][:capture_rate]
-    pokemon.flee_rate = params[:pokemon][:flee_rate]
-    pokemon.pokedex_entry = params[:pokemon][:pokedex_entry]
-    pokemon.comment = params[:pokemon][:comment]
-    pokemon.eggs = Egg.where(id: params[:pokemon][:egg_ids]) unless Egg.where(id: params[:pokemon][:egg_ids]).empty? 
-    pokemon.types = Type.where(id: params[:pokemon][:type_ids]) unless Type.where(id: params[:pokemon][:type_ids]).empty?
-    pokemon.attacks = Attack.where(id: params[:pokemon][:attack_ids]) unless Attack.where(id: params[:pokemon][:attack_ids]).empty?
-    pokemon.save
-    if pokemon.save
+    @pokemon = Pokemon.find(params[:id])
+    @pokemon.update(pokemon_params)
+    if @pokemon.update(pokemon_params)
       redirect_to admin_pokemons_path
     else
-      redirect_to edit_admin_pokemon_path(pokemon)
+      render 'edit'
     end
   end
 
