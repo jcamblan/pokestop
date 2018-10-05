657be3a6ef06fb6203be201bbb52a87a79b1a170
Julien Camblan <julien.camblan@gmail.com>
2018-10-05 13:02:49 +0200

    D'abord, on gère le calcul du DPS de chaque attaque

--- app/models/attack.rb
+++ app/models/attack.rb
@@ -10,4 +10,13 @@ class Attack < ApplicationRecord
   validates :epu, numericality: true, :allow_nil => true
   validates :energy_bars, numericality: { only_integer: true }, :allow_nil => true
 
+
+  # STAB (Same Type Pokemon Bonus) : bonus de 20% aux dégâts si le type du pokémon est le même
+  # que celui de l'attaque
+  def dps(*stab_test)
+    dps = (self.power / self.cast_time)
+    dps = dps *= 1.2 if stab_test.first && stab_test.first.types.include?(self.type)
+    return dps.round(1)
+  end
+
 end
