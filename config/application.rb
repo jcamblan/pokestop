d5dd713d144a390a4e9022fc522edae52290dfc3
Julien Camblan <julien.camblan@gmail.com>
2018-10-10 11:25:33 +0200

    modif de config des assets pour heroku

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module Pokestop
     config.autoload_paths += ["#{config.root}/app/services"]
 
     config.i18n.default_locale = :'fr'
+    config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
