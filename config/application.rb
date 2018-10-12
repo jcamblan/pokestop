6e1ee288d30e0cdec28758d7104ab6ee98dfc863
Julien Camblan <julien.camblan@gmail.com>
2018-10-12 16:28:01 +0200

    activation de la personnalisation des pages d'erreur

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module Pokestop
 
     config.i18n.default_locale = :'fr'
     config.serve_static_assets = true
+    config.exceptions_app = self.routes
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
