a82e398dd22ac9ccaa201d8193032c85db4ab10b
Julien Camblan <julien.camblan@gmail.com>
2018-10-17 09:19:35 +0200

    modif de la timezone

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -25,6 +25,10 @@ class Pokemon < ApplicationRecord
   extend FriendlyId
   friendly_id :name, use: [:slugged, :finders]
 
+  def should_generate_new_friendly_id?
+    name_changed?
+  end
+  
   def touch_updated_at(habtm)
     self.touch if persisted?
   end
--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module Pokestop
     config.i18n.default_locale = :'fr'
     config.serve_static_assets = true
     config.exceptions_app = self.routes
+    config.time_zone = 'Paris'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
